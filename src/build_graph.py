98358f9e4d7620c6df1895d7938bac9712f8cec3
James Ko <jamesqko@gmail.com>
2018-09-09 16:06:20 -0400

    Work

--- src/build_graph.py
+++ src/build_graph.py
@@ -11,29 +11,23 @@ def init_weights(n_inputs, n_outputs, name):
 def init_biases(n_outputs, name):
     return tf.get_variable(tf.zeros([n_outputs]), name=name)
 
-class NNLayer(object):
-    def __init__(self, prev, n_outputs, name):
-        self._prev = prev
-
-        n_inputs = prev.shape[1]
-        self.shape = [n_inputs, n_outputs]
-
-        self.input = prev.output if isinstance(prev, NNLayer) else prev
-        self.weights = init_weights(n_inputs, n_outputs, name=(name + '_weights'))
-        self.biases = init_biases(n_outputs, name=(name + '_biases'))
-        self.output = tf.matmul(self.input, self.weights) + self.biases
-
-def build_nn():
-    input = tf.placeholder(dtype=tf.float64,
-                           name='input') # TODO: shape
-    layer1 = NNLayer(input, n_outputs=300, name='layer1')
-    layer2 = NNLayer(layer1, n_outputs=100, name='layer2')
-    output = NNLayer(layer2, n_outputs=1, name='output')
-    return output
+def hidden_layer(input, n_outputs, name):
+    n_inputs = input.shape[1]
+    weights = init_weights(n_inputs, n_outputs, name=(name + '_weights'))
+    biases = init_biases(n_outputs, name=(name + '_biases'))
+    return tf.add(tf.matmul(input, weights), biases)
 
 def main():
-    nn = build_nn()
-    nn.output[0][0].eval()
+    with tf.variable_scope('Qhat'):
+        input = tf.placeholder(dtype=tf.float64,
+                               name='input') # TODO: shape
+        layer1 = hidden_layer(input, n_outputs=300, name='layer1')
+        layer2 = hidden_layer(layer1, n_outputs=100, name='layer2')
+        Qhat = hidden_layer(layer2, n_outputs=1, name='output')
+
+    with tf.variable_scope('max_Qhat'):
+        inputs = tf.placeholder(dtype=tf.float64,
+                                name='inputs') # TODO: Shape
 
 if __name__ == '__main__':
     main()
